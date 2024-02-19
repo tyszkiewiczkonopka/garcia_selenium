ea33d2d4a58a191c659fc21cf8fc3ed46bc0d516
Magdalena <tyszkiewicz.konopqa@gmail.com>
2024-02-19 16:48:35 +0100

    Add Hello World initial test for Chapter 2

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class HelloWorld {
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class HelloWorld {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void test() {
+        String url = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(url);
+        String pageTitle = driver.getTitle();
+
+        log.debug("The title of {} is {}", url, pageTitle);
+
+        assertThat(pageTitle).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
 }
